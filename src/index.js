03d89792320f52285b104006dd6ccd7fd23b4f07
Daniele <daniele.tortora@hackney.gov.uk>
2019-05-03 18:20:42 +0100

    app structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './components/App';
 
-function HI() {
+function Hi() {
 	return <p>Hi</p>;
 }
 
-render(<HI />, document.getElementById('app'));
+render(
+	<Router>
+		<App />
+	</Router>,
+	document.getElementById('app')
+);
