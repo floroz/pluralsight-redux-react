133de8f3c1cabf6ac5488a89eeaff4c72cdecce6
Daniele <daniele.tortora@hackney.gov.uk>
2019-05-04 17:22:37 +0100

    async writing to mock database

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -12,12 +12,41 @@ export const loadCoursesSuccess = courses => {
 	};
 };
 
+export const updateCourseSuccess = course => {
+	return {
+		type: actionType.UPDATE_COURSE_SUCCESS,
+		course
+	};
+};
+
+export const createCourseSuccess = course => {
+	return {
+		type: actionType.CREATE_COURSE_SUCCESS,
+		course
+	};
+};
+
 export const loadCourses = () => {
 	return dispatch => {
 		return courseApi
 			.getCourses()
 			.then(courses => {
 				dispatch(loadCoursesSuccess(courses));
+			})
+			.catch(error => {
+				throw error;
+			});
+	};
+};
+
+export const saveCourse = course => {
+	return dispatch => {
+		return courseApi
+			.saveCourse(course)
+			.then(savedCourse => {
+				course.id
+					? dispatch(updateCourseSuccess(savedCourse))
+					: dispatch(createCourseSuccess(savedCourse));
 			})
 			.catch(error => {
 				throw error;
--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -3,8 +3,10 @@ import initialState from './initialState';
 
 export default function courseReducer(state = initialState.courses, action) {
 	switch (action.type) {
-		case actionType.CREATE_COURSE:
+		case actionType.CREATE_COURSE_SUCCESS:
 			return state.concat(action.course);
+		case actionType.UPDATE_COURSE_SUCCESS:
+			return state.map(c => (c.id === action.course.id ? action.course : c));
 		case actionType.LOAD_COURSES_SUCCESS:
 			return action.courses;
 		default:
