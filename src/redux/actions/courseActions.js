d78e39a532bd001b443b37c057c2ae94b53594ed
Daniele <daniele.tortora@hackney.gov.uk>
2019-05-04 12:24:59 +0100

    add think and configure async middleware

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,5 +1,26 @@
 import * as actionType from './actionTypes';
+import * as courseApi from '../../api/courseApi';
 
-export function createCourse(course) {
+export const createCourse = course => {
 	return { type: actionType.CREATE_COURSE, course };
-}
+};
+
+export const loadCoursesSuccess = courses => {
+	return {
+		type: actionType.LOAD_COURSES_SUCCESS,
+		courses
+	};
+};
+
+export const loadCourses = () => {
+	return dispatch => {
+		return courseApi
+			.getCourses()
+			.then(courses => {
+				dispatch(loadCoursesSuccess(courses));
+			})
+			.catch(error => {
+				throw error;
+			});
+	};
+};
--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -4,6 +4,8 @@ export default function courseReducer(state = [], action) {
 	switch (action.type) {
 		case actionType.CREATE_COURSE:
 			return state.concat(action.course);
+		case actionType.LOAD_COURSES_SUCCESS:
+			return action.courses;
 		default:
 			return state;
 	}
