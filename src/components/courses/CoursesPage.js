9bbf624d30ed4516a64d93dfbfdf5a6d7bbdd74e
Daniele <daniele.tortora@hackney.gov.uk>
2019-05-04 12:54:29 +0100

    courses add

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,37 +1,67 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import * as courseActions from '../../redux/actions/courseActions';
+import * as authorActions from '../../redux/actions/authorActions';
 import PropTypes from 'prop-types';
-import { bindActionCreators } from 'redux';
+import CourseList from './CourseList';
 
 class CoursesPage extends React.Component {
+	componentDidMount() {
+		const { courses, authors, loadAuthors, loadCourses } = this.props;
+		if (courses.length === 0) {
+			try {
+				loadCourses();
+			} catch (error) {
+				console.log('Loading courses failed: ' + error);
+			}
+		}
+
+		if (authors.length === 0) {
+			try {
+				loadAuthors();
+			} catch (error) {
+				console.log('Authors courses failed: ' + error);
+			}
+		}
+	}
+
 	render() {
 		return (
 			<>
-				<h2>Course</h2>
+				<h2>Courses</h2>
 
-				{this.props.courses.map(course => (
-					<div key={course.title}>{course.title}</div>
-				))}
+				<CourseList courses={this.props.courses} />
 			</>
 		);
 	}
 }
 
 CoursesPage.propTypes = {
-	actions: PropTypes.object.isRequired,
-	courses: PropTypes.array.isRequired
+	courses: PropTypes.array.isRequired,
+	authors: PropTypes.array.isRequired,
+	loadAuthors: PropTypes.func.isRequired,
+	loadCourses: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => {
 	return {
-		courses: state.courses
+		courses:
+			state.authors.length === 0
+				? []
+				: state.courses.map(course => {
+						return {
+							...course,
+							authorName: state.authors.find(a => a.id === course.authorId).name
+						};
+				  }),
+		authors: state.authors
 	};
 };
 
 const mapDispatchToProps = dispatch => {
 	return {
-		actions: bindActionCreators(courseActions, dispatch)
+		loadCourses: () => dispatch(courseActions.loadCourses()),
+		loadAuthors: () => dispatch(authorActions.loadAuthors())
 	};
 };
 
