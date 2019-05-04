87ee17cdd21e5adf732ddacc5acd7e91f3c022b3
Daniele <daniele.tortora@hackney.gov.uk>
2019-05-04 12:57:34 +0100

    initialState

--- src/redux/reducers/authorReducers.js
+++ src/redux/reducers/authorReducers.js
@@ -1,6 +1,7 @@
 import * as actionType from '../actions/actionTypes';
+import initialState from './initialState';
 
-export default function authorReducer(state = [], action) {
+export default function authorReducer(state = initialState, action) {
 	switch (action.type) {
 		case actionType.LOAD_AUTHORS_SUCCESS:
 			return action.authors;
--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,6 +1,7 @@
 import * as actionType from '../actions/actionTypes';
+import initialState from './initialState';
 
-export default function courseReducer(state = [], action) {
+export default function courseReducer(state = initialState, action) {
 	switch (action.type) {
 		case actionType.CREATE_COURSE:
 			return state.concat(action.course);
