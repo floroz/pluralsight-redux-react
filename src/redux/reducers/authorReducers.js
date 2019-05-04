75771bbb6e501446cefe0515e96936fe170a31eb
Daniele <daniele.tortora@hackney.gov.uk>
2019-05-04 14:33:36 +0100

    fix bug init state only slices for reducer

--- src/redux/reducers/authorReducers.js
+++ src/redux/reducers/authorReducers.js
@@ -1,7 +1,7 @@
 import * as actionType from '../actions/actionTypes';
 import initialState from './initialState';
 
-export default function authorReducer(state = initialState, action) {
+export default function authorReducer(state = initialState.authors, action) {
 	switch (action.type) {
 		case actionType.LOAD_AUTHORS_SUCCESS:
 			return action.authors;
--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,7 +1,7 @@
 import * as actionType from '../actions/actionTypes';
 import initialState from './initialState';
 
-export default function courseReducer(state = initialState, action) {
+export default function courseReducer(state = initialState.courses, action) {
 	switch (action.type) {
 		case actionType.CREATE_COURSE:
 			return state.concat(action.course);
